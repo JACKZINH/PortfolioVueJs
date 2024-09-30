680f54d31b7224d5c19abc8076c605c756c46dd1
JACKZINH <clemsir24@gmail.com>
2024-09-30 14:21:15 +0200

    Création Modale
    création de modale.vue et de contenu.vue

---
+++ src/components/Contenu.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="contenu">
+    <h2>CONTENU</h2>
+    <div class="line-left"></div>
+    <div class="contenu-grid">
+      <div>
+        <img
+          src="@/assets/img/image cv .png"
+          class="cv"
+          alt="Curriculum Vitae" />
+        <p>Curriculum vitae</p>
+        <p>Techno utilisée : HTML, CSS</p>
+      </div>
+      <div>
+        <img
+          src="@/assets/img/image cahier des charges.png"
+          class="cahier"
+          alt="Cahier des charges" />
+        <p>Cahier des charges</p>
+        <p>Techno utilisée : HTML, CSS</p>
+      </div>
+      <div>
+        <img
+          src="@/assets/img/image dynamiser.png"
+          class="dynamiser"
+          alt="Dynamiser un espace commentaire" />
+        <p>Dynamiser un espace commentaire</p>
+        <p>Techno utilisée : HTML, CSS, JavaScript</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Contenu",
+};
+</script>
+
+<style></style>
---
+++ src/components/Modale.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="modale">
+    <div class="modale card"></div>
+    <div class="modale-overlay" @click="closeModale"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Modale",
+  methods: {
+    closeModale() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
+
+<style></style>
